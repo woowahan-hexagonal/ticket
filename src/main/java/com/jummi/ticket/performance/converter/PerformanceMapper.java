b8d513217bd5dffcd318dde0a07a8a95ffd21d0c
jummi <98qkrwjdal@naver.com>
2022-06-23 03:45:04 +0900

    fix: mapper가 자동매핑 못해주는 필드 직접 매핑

--- src/main/java/com/jummi/ticket/performance/converter/PerformanceMapper.java
+++ src/main/java/com/jummi/ticket/performance/converter/PerformanceMapper.java
@@ -15,6 +15,10 @@ import java.util.stream.Collectors;
 @Component
 @Mapper
 public interface PerformanceMapper {
+
+    @Mapping(expression = "java(performance.getPerformanceId().id())", target = "performanceId")
+    @Mapping(expression = "java(performance.getPeriod().startDate())", target = "startDate")
+    @Mapping(expression = "java(performance.getPeriod().endDate())", target = "endDate")
     PerformanceEntity convertDomainEntityToJpaEntity(Performance performance);
 
     default Performance convertRequestToDomainEntity(RegisterPerformanceRequest request) {
@@ -23,20 +27,17 @@ public interface PerformanceMapper {
                 .genre(Genre.valueOf(request.getGenre()))
                 .period(new Period(request.getStartDate(), request.getEndDate()))
                 .venue(request.getVenue())
-                .casts(request.getCasts())
                 .runTime(request.getRunTime())
                 .minAvailableAge(request.getMinAvailableAge())
                 .reservationSites(convertStringToEnum(request.getReservationSites()))
                 .build();
     }
 
-    default List<ReservationSite> convertStringToEnum(List<String> reservationSites) {
+    private List<ReservationSite> convertStringToEnum(List<String> reservationSites) {
         return reservationSites.stream()
                 .map(ReservationSite::valueOf)
                 .collect(Collectors.toList());
     }
-
-    List<SeriesEntity> convertDomainEntityToJpaEntity(List<Series> series);
 
     default List<Series> extractSeriesDomainEntity(RegisterPerformanceRequest request) {
         return request.getSeries()
@@ -45,8 +46,14 @@ public interface PerformanceMapper {
                 .collect(Collectors.toList());
     }
 
-    @Mapping(expression = "java(seriesRequest.getDateTime().toLocalDate())", target = "playDate")
-    @Mapping(expression = "java(seriesRequest.getDateTime().toLocalTime())", target = "playTime")
-    @Mapping(expression = "java(seriesRequest.isPerformed())", target = "isPerformed")
-    Series convertRequestToDomainEntity(SeriesRequest seriesRequest);
+    private Series convertRequestToDomainEntity(SeriesRequest seriesRequest) {
+        return Series.builder()
+                .playDate(seriesRequest.getDateTime().toLocalDate())
+                .playTime(seriesRequest.getDateTime().toLocalTime())
+                .casts(seriesRequest.getCasts())
+                .isPerformed(seriesRequest.isPerformed())
+                .build();
+    }
+
+    List<SeriesEntity> convertDomainEntityToJpaEntity(List<Series> series);
 }
